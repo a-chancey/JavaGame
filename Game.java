2d18fd11e75a6593d8b98347703b600bd2656495
Aaron Chancey <abchiptop@gmail.com>
2015-08-19 09:33:09 -0400

    Initial Commit
    Initial Commit for this little java "game".  Command line app that will
    let us reverse a string, add two numbers or tell if the input number is
    prime.

---
+++ Game.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Game {
+	public static void main(String[] args) {
+		System.out.println("Shall we play a game?");
+		System.out.println("Press 1 for string reverser.");
+		System.out.println("Press 2 for number adder.");
+		System.out.println("Press 3 for prime number finder:");
+		Scanner input = new Scanner(System.in);
+		int i = input.nextInt();
+		
+		System.out.println(i);
+	}
+}
