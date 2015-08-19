f0a5795f58c58ed7026829710b30a419788a0a00
Aaron Chancey <abchiptop@gmail.com>
2015-08-19 10:56:16 -0400

    Added StringReverser and Test
    Added our first game - string reverser - and the relevant unit test.
    Adder comes next.

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,16 +1,57 @@
 package game;
 
 import java.util.*;
+import java.lang.*;
 
 class Game {
-	public static void main(String[] args) {
-		System.out.println("Shall we play a game?");
-		System.out.println("Press 1 for string reverser.");
-		System.out.println("Press 2 for number adder.");
-		System.out.println("Press 3 for prime number finder: \n");
-		Scanner input = new Scanner(System.in);
-		int i = input.nextInt();
-		
-		System.out.println(i);
-	}
+    public static void main(String[] args) {
+        while (true) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.println("Shall we play a game?");
+            System.out.println("Press 1 for string reverser.");
+            System.out.println("Press 2 for number adder.");
+            System.out.println("Press 3 for prime number finder: \n");
+
+            int i = input.nextInt();
+
+            switch (i) {
+                case 1:
+                    System.out.println("Welcome to the String Reverser");
+                    stringGame();
+                    break;
+                case 2:
+                    addNums();
+                    break;
+                case 3:
+                    findPrime();
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again");
+            }
+        }
+    }
+
+    private static void findPrime() {
+    }
+
+    private static void addNums() {
+    }
+
+    private static void stringGame() {
+        while (true) {
+            System.out.println("Please type a string:");
+            Scanner stringy = new Scanner(System.in);
+            String inputString = stringy.nextLine();
+
+            String output = stringReverser(inputString);
+            
+            System.out.println("Your reversed string is " + output);
+        }
+    }
+
+    public static String stringReverser(String inputString) {
+        String reversed = new StringBuilder(inputString).reverse().toString();
+        return reversed;
+    }
 }
---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,29 @@
+package game;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * Created by Aaron on 8/19/2015.
+ */
+public class GameTest {
+    Game game;
+
+    @Before
+    public void SetUp(){
+        game = new Game();
+    }
+
+    @Test
+    public void StringReverserTest(){
+        String input = "Test";
+        String result;
+
+        result = Game.stringReverser(input);
+        assertEquals("tseT", result);
+    }
+
+
+
+}
